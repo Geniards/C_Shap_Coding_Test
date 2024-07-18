44fe285619c3889eadca1eb06f681657107118e7
Geniards <geniard93@gmail.com>
2024-07-18 22:07:10 +0900

    c#프로그래머스 기초트레이닝
    List.RemoveAt() 사용

---
+++ code_test/프로그래머스_C#_기초_입문/배열 만들기 4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code_test.프로그래머스_C__기초_입문
+{
+    internal class 배열_만들기_4
+    {
+        #region  배열 만들기 4   List.RemoveAt(지정 인덱스)제거
+        //static int[] solution(int[] arr)
+        //{
+        //    int[] answer = new int[] { };
+
+        //    List<int> list = new List<int>();
+
+        //    for (int i = 0; i < arr.Length;)
+        //    {
+        //        if (list.Count == 0)
+        //        {
+        //            list.Add(arr[i]);
+        //            i++;
+        //        }
+        //        else if (list[list.Count - 1] < arr[i])
+        //        {
+        //            list.Add(arr[i]);
+        //            i++;
+        //        }
+        //        else if (list[list.Count - 1] >= arr[i])
+        //            list.RemoveAt(list.Count - 1);
+        //    }
+
+        //    answer = list.ToArray();
+
+        //    return answer;
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    int[] a = { 1, 4, 2, 5, 3 };
+
+        //    Console.WriteLine($"{solution(a)}");
+
+        //}
+        #endregion
+    }
+}
