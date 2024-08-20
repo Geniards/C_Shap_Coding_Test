2af6bb9f77e61d9a0184c31e30d446e9f8580754
Geniards <geniard93@gmail.com>
2024-08-20 21:22:44 +0900

    Solved:240820
    C# 코딩테스트

---
+++ code_test/백준/팰린드롬수/팰린드롬수(1259).cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code_test.백준.팰린드롬수
+{
+    public class 팰린드롬수_1259_
+    {
+        public void solution()
+        {
+            List<string> sb = new List<string>();
+
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (input == null)
+                {
+                    throw new Exception();
+                }
+
+                if (input == "0")
+                    break;
+
+                char[] chars = input.ToCharArray();
+
+                Array.Reverse(chars);
+                string str = new string(chars);
+
+                if (input == str)
+                    sb.Add("yes");
+                else
+                    sb.Add("no");
+            }
+
+            foreach (string s in sb)
+                Console.WriteLine(s);
+
+        }
+    }
+}
--- 팰린드롬수(1259)/Program.cs
+++ 팰린드롬수(1259)/Program.cs
@@ -4,32 +4,6 @@
     {
         static void Main(string[] args)
         {
-            List<string> sb = new List<string>();
-
-            while (true)
-            {
-                string? input = Console.ReadLine();
-                if (input == null)
-                {
-                    throw new Exception();
-                }
-
-                if (input == "0")
-                    break;
-
-                char[] chars = input.ToCharArray();
-
-                Array.Reverse(chars);
-                string str = new string(chars);
-
-                if (input == str)
-                    sb.Add("yes");
-                else
-                    sb.Add("no");
-            }
-
-            foreach (string s in sb)
-                Console.WriteLine(s);
 
         }
     }
